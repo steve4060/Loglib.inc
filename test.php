e2a92db8e4f47c1e6e644d62a9165bce00250afe
steve4060 <steve.4060.ri@gmail.com>
2024-12-07 14:42:08 +0100

    mad time optional

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
 include ('loglib.inc');
-$log = new Logtype('file');
+$log = new Logtype('file', false);
+$log2 = new Logtype('file');
 Log::initLoging($log);
-Log::write($log, 'test', 'info')
+Log::write($log, 'Info test', 'info');
+Log::write($log, 'Warning test', 'warning');
+Log::write($log, 'Error test', 'error');
+
+Log::initLoging($log2);
+Log::write($log2, 'Info test', 'info');
+Log::write($log2, 'Warning test', 'warning');
+Log::write($log2, 'Error test', 'error');
 ?>
